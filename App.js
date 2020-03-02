01030b176489499e883672f68cc869e22d5feba7
1986webdeveloper <1986webdeveloper@gmail.com>
2020-03-02 10:42:05 +0530

    multidex error add multidex on android application class

--- App.js
+++ App.js
@@ -1,29 +1,30 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import * as TableSDK from './TableSDK';
+import {Text, View, Button} from 'react-native';
+import * as TableSDK from './src/TableSDK';
 
 export default class App extends Component {
   componentDidMount() {
-    TableSDK.init.workspaceUrl = 'develop3.dev';
-    TableSDK.init.apiKey = 'asasasas';
-    TableSDK.init.userHash = 'asas';
+    TableSDK.init('develop3.dev', 'asasasas', 'asas');
+  }
+  onButtonPres = () => {
     var tableParams = {
       email: 'felixthomas727@gmail.com',
       firstName: 'Felix',
       lastName: 'Thomas',
       custom_attributes: {},
     };
-
     TableSDK.register('asas', 'aas', tableParams)
       .then(response => {
         alert(JSON.stringify(response));
       })
-      .catch(error => {
-        alert(JSON.stringify(error));
-      });
-  }
+      .catch(error => {});
+  };
 
   render() {
-    return <View></View>;
+    return (
+      <View>
+        <Button onPress={this.onButtonPres} title="call "></Button>
+      </View>
+    );
   }
 }
--- src/TableSDK.js
+++ src/TableSDK.js
@@ -1,22 +1,29 @@
 import AsyncStorage from '@react-native-community/async-storage';
-
-export const init = {workspaceUrl: '', apiKey: '', userHash: ''};
-
+export const init = (workspce, apiKey, userHash) => {
+  global.workspaceUrl = 'https://' + workspce + '.table.co/';
+  global.apiKey = apiKey;
+  global.userHash = userHash;
+};
+export const showConversationList = () => {
+  AsyncStorage.getItem('data').then(data => {
+    if (data) {
+    }
+  });
+};
+export const logout = () => {
+  AsyncStorage.removeItem('data').then(data => {});
+};
 export const register = (userID, userHash, tableParams) => {
   return new Promise(function(resolve, reject) {
-    var url =
-      'https://' +
-      init.workspaceUrl +
-      '.table.co/user-service/user/auth/chat-user';
-    init.workspaceUrl = url;
-    fetch(init.workspaceUrl, {
+    var url = global.workspaceUrl + 'user-service/user/auth/chat-user';
+    fetch(url, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         user_id: userID,
         email: tableParams.email,
-        api_key: init.apiKey,
-        user_hash: init.userHash,
+        api_key: global.apiKey,
+        user_hash: global.userHash,
         first_name: tableParams.first_name,
         last_name: tableParams.last_name,
         custom_attributes: tableParams.custom_attributes,
