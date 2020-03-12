8bce2dd02779d868a567071e23c5d4904751383e
applegold <dbmsidley@gmail.com>
2020-03-12 11:00:10 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as TableSDK from './lib/TableSDK';
 export {TableSDK};
 export {TableScreen} from './lib/TableScreen';
-// export {Setting} from './lib/Setting';
-// export {Video} from './lib/Video';
+export {Setting} from './lib/Setting';
+export {Video} from './lib/Video';
