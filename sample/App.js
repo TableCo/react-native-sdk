af0a2a221cbe5e67fb495ac877529c6a45fe69ff
Gareth Reese <gazreese@gmail.com>
2020-03-18 20:05:33 +0000

    Version 0.2.0. Tidy up and more explicit token management in the Dashboard

--- sample/App.js
+++ sample/App.js
@@ -1,7 +1,6 @@
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Welcome from './src/Welcome';
-// import {Dashboard, Setting, Video} from 'table-react-native-sdk';
 import {TableScreen} from 'table-react-native-sdk';
 
 const AuthStack = createStackNavigator(
